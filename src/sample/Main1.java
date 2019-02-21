aeec43025e09d8237af4ef7d0fca92a3fe402aba
Gast9ra <mavrovasili@gmail.com>
2019-02-21 17:33:16 +0300

    new data

--- src/sample/Main1.java
+++ src/sample/Main1.java
@@ -5,7 +5,9 @@ public class Main1 {
 
     public static void main(String[] args) throws Exception {
         CalcData cal = new CalcData();
-        double[] temp = cal.map("g4-9.3", "h4-4.3");
+        double[] temp = cal.map("g4-1.1", "g4-10.1");
+        System.out.println(temp[0]);
+        System.out.println(temp[1]);
 //        Scanner read = new Scanner(System.in);
 //        System.out.println("Пример: A1-3-5-1");
 //       // map("a1-2-6-4","a1-2-1-5");
